d70cfaccb17651ff4efb44803f710f8af150cf7d
tianshujian <tianshujian@idstaff.com>
2019-12-10 11:22:04 +0800

    简化NamedThreadFactory

--- src/main/java/com/tianye/helloejob/executor/NamedThreadFactory.java
+++ src/main/java/com/tianye/helloejob/executor/NamedThreadFactory.java
@@ -10,29 +10,19 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class NamedThreadFactory implements ThreadFactory {
 
-    private static final AtomicInteger poolNumber = new AtomicInteger(1);
-    private final ThreadGroup group;
     private final AtomicInteger threadNumber = new AtomicInteger(1);
     private final String namePrefix;
+    private final boolean daemon;
 
     NamedThreadFactory(ThreadPoolEnum threadPoolEnum) {
-        SecurityManager s = System.getSecurityManager();
-        group = (s != null) ? s.getThreadGroup() :
-                Thread.currentThread().getThreadGroup();
-        namePrefix = threadPoolEnum.getDesc()
-                + "-pool-"
-                + poolNumber.getAndIncrement()
-                + "-";
+        namePrefix = threadPoolEnum.getDesc() + "-thread-";
+        daemon = false;
+
     }
 
     public Thread newThread(Runnable r) {
-        Thread t = new Thread(group, r,
-                namePrefix + threadNumber.getAndIncrement(),
-                0);
-        if (t.isDaemon())
-            t.setDaemon(false);
-        if (t.getPriority() != Thread.NORM_PRIORITY)
-            t.setPriority(Thread.NORM_PRIORITY);
+        Thread t = new Thread( r, namePrefix + threadNumber.getAndIncrement());
+        t.setDaemon(daemon);
         return t;
     }
 }
